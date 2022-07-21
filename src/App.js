b7dc0fd8b4d859258abcbe91661b33e3beccfa26
leegyuseung <lgs4002@naver.com>
2022-07-22 03:11:44 +0900

    사용자 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DiaryEditor />
+    </div>
+  );
 }
 
 export default App;
